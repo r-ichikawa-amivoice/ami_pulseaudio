798dd50c0b592fecc8e14a81229720fb8bba0985
r-ichikawa <r-ichikawa@advanced-media.co.jp>
2021-06-28 14:48:37 +0900

    どこのパスから動かされても大丈夫なように

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib/
 echo "Press the enter key to exit."
 ./bin/ami_pulseaudio | aplay -f cd -c 1 -r 16000
