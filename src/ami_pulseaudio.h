e230e5f19af1e8d44eba2df5b04954f368131f58
r-ichikawa-amivoice <r-ichikawa@advanced-media.co.jp>
2021-07-13 14:00:15 +0900

    コメントをdocっぽく

--- src/ami_pulseaudio.h
+++ src/ami_pulseaudio.h
@@ -21,7 +21,9 @@
 extern "C" {
 #endif
 
-//ステータス
+/** @enum AMI_PULSEAUDIO_RESULT_STATE
+ *  AMI_PULSEAUDIOのステータス
+ */
 enum AMI_PULSEAUDIO_STATE {
 	//! ERROR
 	AMI_PULSEAUDIO_STATE_ERROR = -1,
@@ -33,41 +35,74 @@ enum AMI_PULSEAUDIO_STATE {
 	AMI_PULSEAUDIO_STATE_RECORDING = 2,
 };
 
-//callbackのステータス
+/callbackのステータス
+/** @enum AMI_PULSEAUDIO_RESULT_STATE 
+ *  callbackのステータス
+ *  コールバックの処理はこの値を見てから処理をすると良い
+ */
 enum AMI_PULSEAUDIO_RESULT_STATE {
-	//エラー
+	//! エラー
 	AMI_PULSEAUDIO_RESULT_STATE_ERROR = -1,
-	//ストップ
+	//! ストップ
 	AMI_PULSEAUDIO_RESULT_STATE_STOPPED = 0,
-	//スタート
+	//! スタート
 	AMI_PULSEAUDIO_RESULT_STATE_STARTED = 1,
-	//録音できたよ
+	//! 録音できたよ
 	AMI_PULSEAUDIO_RESULT_STATE_ACCEPTED = 2,
 };
 
+/**
+ * @typedef
+ * 録音イベントのコールバック関数
+ * @property (result_state) AMI_PULSEAUDIO_RESULT_STATE
+ * @property (data_size) dataの長さ
+ * @property (data) PCM 16kHz 1ch の音 
+ */
 typedef void (*ami_pulseaudio_callback_func)(enum AMI_PULSEAUDIO_RESULT_STATE result_state, int data_size, char* data);
 
-//初期化
-//0:ng
-//それ以外:ポインター
+/**
+ * @fn
+ * 初期化
+ * @brief 録音変数の初期化
+ * @param (callback) コールバック関数
+ * @return 0:NG それ以外:録音変数のポインター
+ */
 void* ami_pulseaudio_create(ami_pulseaudio_callback_func callback);
 
-//破棄
-//0:ng
-//1:ok
+/**
+ * @fn
+ * 破棄
+ * @brief 録音変数の破棄
+ * @param (ap) 録音変数
+ * @return 0:NG 1:ok
+ */
 int ami_pulseaudio_free(void* ap);
 
-//録音開始
-//0:ng
-//1:ok
+/**
+ * @fn
+ * 録音開始
+ * @brief 録音の開始
+ * @param (ap) 録音変数
+ * @return 0:NG 1:ok
+ */
 int ami_pulseaudio_start(void* ap);
 
-//録音終了
-//0:ng
-//1:ok
+/**
+ * @fn
+ * 録音終了
+ * @brief 録音の終了
+ * @param (ap) 録音変数
+ * @return 0:NG 1:ok
+ */
 int ami_pulseaudio_stop(void* ap);
 
-//現在のステータス
+/**
+ * @fn
+ * 現在のステータス
+ * @brief 現在のステータスの取得
+ * @param (ap) 録音変数
+ * @return AMI_PULSEAUDIO_STATE
+ */
 enum AMI_PULSEAUDIO_STATE ami_pulseaudio_get_state(void* ap);
 
 #ifdef __cplusplus
